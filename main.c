2371f71f10d55a1522cf1b770bd057e2d70925c1
Filip <filipporeba967@gmail.com>
2022-09-18 11:01:58 +0200

    isLeap cheaking

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void checkValue(double a){
+void checkValue(double a) {
     if (a > 0) {
         printf("ta liczba jest dodatnia\n");
     } else if (a < 0) {
@@ -11,7 +11,22 @@ void checkValue(double a){
 
 }
 
+//napisz funkcje ktora przyjmie rok i wydrukuje czy jest przestepny czy nie
+void isLeap(int year) {
+    if (year % 400 == 0) {
+        printf("%i to rok przestepny", year);
+    } else if (year % 100 == 0) {
+        printf("%i nie jest rokiem przestepnym", year);
+    } else if (year % 4 == 0) {
+        printf("%i to rok przestepny\n", year);
+    } else {
+        printf("%i nie jest rokiem przestepnym\n", year);
+    }
+}
+
 int main() {
-    checkValue(-43);
+    // checkValue(-43);
+    isLeap(2000);
     return 0;
 }
+
