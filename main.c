a20acaa98a90249226f9113630856afa4316e243
Filip <filipporeba967@gmail.com>
2022-11-08 16:02:19 +0100

    boubbleSortfunction

--- main.c
+++ main.c
@@ -64,14 +64,41 @@ int findMinimum(int x[], int size) {
     return min;
 }
 
+void swap(int *f, int *i) {
+    int temp = *f;
+    *f = *i;
+    *i = temp;
+}
+
+void bubbleSort(int a[], int size) {
+    int i, j;
+    for (i = 0; i < size - 1; i++)
+
+        for (j = 0; j < size - i - 1; j++)
+            if (a[j] > a[j + 1])
+                swap(&a[j], &a[j + 1]);
+}
+
+void array(int arr[], int size) {
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
 int main() {
     //checkValue();
     //isLeap();
     //printEvenNumbers();
     //isLetter('');
-    int d[] = {2, 7, 6, 8, -10, 9};
-    int size = sizeof d / 4;
-    int result = findMinimum(d, size);
-    printf("min = %i", result);
+    //int d[] = {2, 7, 6, 8, -10, 9};
+    //int size = sizeof d / 4;
+    // int result = findMinimum(d, size);
+    //printf("min = %i", result);
+    int arr[] = {64, 34, 25, 12, 22, 11, 90, 76, 23, 87, 97, 31, 54};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    bubbleSort(arr, size);
+    printf("Sorted array: \n");
+    array(arr, size);
     return 0;
 }
